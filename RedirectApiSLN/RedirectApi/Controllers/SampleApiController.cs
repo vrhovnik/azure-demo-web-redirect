6b75193e7c25139c4dab91b51bcaf968498a337a
Bojan Vrhovnik <bojan@vrhovnik.net>
2021-07-23 09:08:11 +0200

    sample API initial check-in

---
+++ RedirectApiSLN/RedirectApi/Controllers/SampleApiController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RedirectApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class SampleApiController : ControllerBase
+    {
+        private readonly ILogger<SampleApiController> logger;
+
+        public SampleApiController(ILogger<SampleApiController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [Route("amialive")]
+        public IActionResult HealthCheck() => Ok($"I'm alive at {DateTime.Now}");
+    }
+}
---
+++ RedirectApiSLN/RedirectClientCalls/Program.cs
@@ -0,0 +1,14 @@
+﻿using Spectre.Console;
+
+namespace RedirectClientCalls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rule = new Rule("Executing rest calls to different API endpoints!");
+            AnsiConsole.Render(rule);
+            //TODO: execute different calls to REST api to demonstrate routing and works with Web API in Linux docker container and Azure FrontDoor
+        }
+    }
+}
